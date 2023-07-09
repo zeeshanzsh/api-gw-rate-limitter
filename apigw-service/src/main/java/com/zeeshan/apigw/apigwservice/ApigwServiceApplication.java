0dc28fd1d528649f9bdb1728f31d60417154be59
Zeeshan <zeeshan@Zeeshans-MacBook-Air.local>
2023-07-10 00:15:20 +0530

    API-GW included with rate limitter for user-service

--- apigw-service/src/main/java/com/zeeshan/apigw/apigwservice/ApigwServiceApplication.java
+++ apigw-service/src/main/java/com/zeeshan/apigw/apigwservice/ApigwServiceApplication.java
@@ -2,12 +2,21 @@ package com.zeeshan.apigw.apigwservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class ApigwServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApigwServiceApplication.class, args);
+	}
+
+	@Bean
+	public KeyResolver keyResolver(){
+		return  exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
+
 	}
 
 }
